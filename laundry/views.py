bc9bff0b510a61750c5610f828a7be80c09fc829
jyeon2yu <45540117+jyeon2yu@users.noreply.github.com>
2021-07-02 21:02:15 +0900

    DB다시!
    DB 다시하자!

--- laundry/views.py
+++ laundry/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
 
+import pandas as pd
+import numpy as np
+
+def laundryDB(request):
+    data = pd.read_csv("landry_data.csv")
+
+    data_landry = data.iloc[:, [3,4,6,11,12,13,14]]
+
+    list_landry = []
+
+    for j in range(len(data_landry)):
+        list_landry.append(list(data_landry.loc[j]))
+
+    
+
+    return render(request,'laundry/laundryDB.html')
 
 def search_map(request):
     return render(request,'laundry/search_map.html')
