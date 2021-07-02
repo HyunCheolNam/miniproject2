8d8dee3a81ce0d75575a64410e93b3a0732a2087
jihye <kokoritaaa7@gmail.com>
2021-07-02 11:05:48 +0900

    [JIHYE branch] Main Page 수정 - 비로그인 시 : '로그인' 출력 - 로그인 시 : 아이콘 + 닉네임 출력 - 로그인 시에만 드롭다운

--- user/views.py
+++ user/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from board import views as b_views
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from user import models as user_model
+import json
 
 def account(request):
     return render(request,'user/account.html')
@@ -10,7 +14,22 @@ def cards(request):
     return render(request,'user/cards.html')
 
 def login(request):
-    return render(request,'user/login.html')
+    ## GET 방식일 때 그냥 화면
+    if request.method == 'GET':
+        return render(request,'user/login.html', {})
+    ## POST 방식일 때 
+    else:
+        user_id = request.POST['user_id']
+        user_pw = request.POST['user_pw']
+        try:
+            user = user_model.User.objects.get(user_id = user_id, user_pw = user_pw)
+            request.session['user_nick'] = user.user_nick
+            request.session['user_id'] = user.user_id
+        except:
+            # return render(request,'user/login.html', {안내메시지})
+            return HttpResponse('로그인 실패')
+        else:
+            return redirect(b_views.main)
 
 def notifications(request):
     return render(request, 'user/notifications.html')
