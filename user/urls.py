ccedcb112863f285fe318eb696066f2337665645
baejjang-i <baejjang_i@kakao.com>
2021-07-02 14:47:49 +0900

    1. 회원가입 페이지 제작 완료 2. Q&A 페이지 문의게시판 추가 완료 3. 지도 페이지 20gk →20kg 오타 수정

--- user/urls.py
+++ user/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('notifications/', views.notifications, name='notifications'),
     path('pwd_reset/', views.pwd_reset, name='pwd_reset'),
+    path('singup/', views.singup, name="singup"),
 ]
